43c12fa6063f00548d80c8a05e1cc9be2a214024
abhijithkanna3-ai <abhijithkanna3@gmail.com>
2026-03-24 23:48:41 +0530

    First commit

---
+++ AeraOfCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AeraOfCircle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius : ");
+        int radius = sc.nextInt();
+        System.out.print("The Area of the Circle is : " + radius*radius*Math.PI);
+    }
+}
